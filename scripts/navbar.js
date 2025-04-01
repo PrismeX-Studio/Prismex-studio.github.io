1e24e7bd4d6dfdc5a1ae756a7fc3fb942e1b1fc3
Starlight7Xe <113340011+Return761@users.noreply.github.com>
2025-04-02 00:59:41 +0800

    fix

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,7 +1,7 @@
 document.write("        <div class=\"b-navbar\">");
 document.write("            <img class=\"n-image\" src=\"..\/images\/nicon.png\">");
 document.write("            <p class=\"t-navbartitle\">SNR Studio<\/p>");
-document.write("            <a href=\"A-1.html\" class=\"t-navbarlink\">首页<\/a>");
+document.write("            <a href=\"home.html\" class=\"t-navbarlink\">首页<\/a>");
 document.write("            <a href=\"#\" class=\"t-navbarlink\" onclick=\"javascript:history.back(-1);\">上页<\/a>");
 document.write("            <a href=\"B-1.html\" class=\"t-navbarlink\">所有文章<\/a>");
 document.write("            <a href=\"..\/special\/snrstartpage\/pages\/A-1.html\" class=\"t-navbarlink\">起始页<\/a>");
