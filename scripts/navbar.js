7776f370c5dbe09feab8cc824be346081e75993d
Starlight7Xe <113340011+Return761@users.noreply.github.com>
2025-04-06 16:53:33 +0800

    update 他妈的LSD主题页面！

--- scripts/navbar.js
+++
@@ -1,7 +0,0 @@
-document.write("        <div class=\"b-navbar\">");
-document.write("            <a href=\"home.html\" class=\"t-navbarlink\">首页<\/a>");
-document.write("            <a href=\"#\" class=\"t-navbarlink\" onclick=\"javascript:history.back(-1);\">上页<\/a>");
-document.write("            <a href=\"B-1.html\" class=\"t-navbarlink\">所有文章<\/a>");
-document.write("            <a href=\"..\/special\/PrismeXstartpage\/pages\/A-1.html\" class=\"t-navbarlink\">起始页<\/a>");
-document.write("        <\/div>");
-document.write("        <p class=\"s-70\"><\/p>");
